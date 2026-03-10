271014d831376303a897bbe48d81842addfbc249
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2026-03-10 11:33:59 +0100

    feat: add energy API type definitions

---
+++ types/energy.ts
@@ -0,0 +1,28 @@
+export interface ProductionType {
+  name: string;
+  data: number[];
+}
+
+export interface PublicPowerData {
+  unix_seconds: number[];
+  production_types: ProductionType[];
+  deprecated: boolean;
+}
+
+export interface PriceData {
+  license_info: string;
+  unix_seconds: number[];
+  price: number[];
+  unit: string;
+  deprecated: boolean;
+}
+
+export interface RenShareForecastData {
+  unix_seconds: number[];
+  ren_share: number[];
+  solar_share: number[];
+  wind_onshore_share: number[];
+  wind_offshore_share: number[];
+  substitute: boolean;
+  deprecated: boolean;
+}
